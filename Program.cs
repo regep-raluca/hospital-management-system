08743f6c345450343f17af7be93cc6801c9cf68a
regep-raluca <raluca09regep@gmail.com>
2025-04-24 20:02:50 +0300

    Fix .gitignore location and remove .vs/ from tracking

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------
+// Program.cs
+// Entry point of the Hospital Management System.
+// Launches the LoginForm.
+// --------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HospitalManagementSystem.Forms
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}
